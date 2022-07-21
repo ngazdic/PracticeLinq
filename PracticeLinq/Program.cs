ab4ee543269097f4d04a7e715fb42b75d8596dfd
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-21 10:48:11 -0400

    practice completed

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+List<string> myList = new List<string>() { "God Of War", "Diablo", "Counter Strike"};
+
+
+
+
+var newVar = myList.OrderByDescending(s => s.Length).ToArray();
+
+foreach (var item in newVar)
+{
+    Console.WriteLine(item);
+}
